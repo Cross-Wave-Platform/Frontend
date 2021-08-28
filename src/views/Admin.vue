ec5b27cee0ae964333eaf8a03771172e3221ae3c
JiewenChan <jiewen1010@gmail.com>
2021-08-28 17:16:54 +0800

    function complete

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -4,22 +4,16 @@
       <v-row no-gutters>
         <v-col cols=2>
           <div style="position: sticky; top: 76px">
-          <v-list shaped>
-            <v-list-item-group color="primary">
-              <v-list-item to="/admin/usermanage">
-                <v-list-item-icon>
-                  <v-icon left>mdi-account</v-icon>
-                </v-list-item-icon>
-                <v-list-item-content>使用者權限管理</v-list-item-content>
-              </v-list-item>
-              <v-list-item to="/admin/datamanage">
-                <v-list-item-icon>
-                  <v-icon left>mdi-lock</v-icon>
-                </v-list-item-icon>
-                <v-list-item-content>資料權限管理</v-list-item-content>
-              </v-list-item>
-            </v-list-item-group>
-          </v-list>
+            <v-tabs vertical>
+                <v-tab class="font-weight-bold" to="/admin/usermanage">
+                    <v-icon left>mdi-account</v-icon>
+                    使用者權限管理
+                </v-tab>
+                <v-tab class="font-weight-bold" to="/admin/datamanage">
+                    <v-icon left>mdi-clipboard-text-multiple</v-icon>
+                    資料權限管理
+                </v-tab>
+            </v-tabs>
           </div>
         </v-col>
         <v-col  cols=10><router-view></router-view></v-col>
