41c0441730b5cf9ba9073f149d9e1d6fce776bee
AndrewL00 <andrew35599588@gmail.com>
2023-05-02 07:01:28 +0900

    complete announcemaent basic in admin and home

--- src/components/AnnounceManage.vue
+++ src/components/AnnounceManage.vue
@@ -1,64 +1,150 @@
 <template>
   <div class="announcemanage">
     <v-card flat style="background-color:rgba(255, 255, 255, 0.0);">
-      <v-form
-          ref="passwordForm"
-          v-model="passwordForm"
-          lazy-validation
-          class="pa-5"
-          @submit.prevent="changePassword"
-        >
-          <v-text-field
-            v-model="password.oldPassword"
-            label="公告標題"
-            :append-icon="showPassword.oldPassword ? 'mdi-eye' : 'mdi-eye-off'"
-            :type="showPassword.oldPassword ? 'text' : 'password'"
-            :rules="[v => !!v || 'Please enter current password.']"
-            @click:append="showPassword.oldPassword = !showPassword.oldPassword"
-            outlined
-          >
-          </v-text-field>
-          <v-text-field
-            v-model="password.newPassword"
-            label="公告內容"
-            :append-icon="showPassword.newPassword ? 'mdi-eye' : 'mdi-eye-off'"
-            :type="showPassword.newPassword ? 'text' : 'password'"
-            :rules="[v => !!v || 'Please enter new password.']"
-            @click:append="showPassword.newPassword = !showPassword.newPassword"
-            outlined
-          >
-          </v-text-field>
-          <v-btn
-            dark
-            color="primary"
-            block
-            type="submit"
-            @click.prevent="changePassword"
-          >
-            儲存
-            <v-icon right>mdi-content-save</v-icon>
+      <v-card-text>
+      <v-data-table
+        :headers="header"
+        :items="announcementList"
+        :items-per-page="5"
+        sort-by="id"
+        style="background-color:rgba(255, 255, 255, 0.0);"
+      >
+        <template v-slot:item.deleteAction="{ item }">
+          <v-btn text @click="deleteAnnouncement(item)">
+            <v-icon>
+              mdi-delete
+            </v-icon>
           </v-btn>
-        </v-form>
+        </template>
+        <template v-slot:item.modifyAction="{ item }">
+          <v-btn text @click="modifyAnnouncement(item)">
+            <v-icon>
+              mdi-pencil
+            </v-icon>
+            編輯
+          </v-btn>
+        </template>
+      </v-data-table>
+      <div>
+        <v-btn
+          color="primary"
+          @click="openCreateDialog"
+        >
+          新增公告
+        </v-btn>
+      </div>
+      </v-card-text>
+      <v-dialog
+        v-model="createDialog"
+        max-width="1000"
+      >
+        <v-card>
+          <v-card-title>
+            新增公告
+          </v-card-title>
+          <v-card-text>
+            <v-textarea
+              v-model="createTitle"
+              counter
+              label="公告標題"
+              rows="3"
+              auto-grow
+            >
+            </v-textarea>
+            <v-textarea
+              v-model="createContents"
+              counter
+              label="公告內容"
+              auto-grow
+            >
+            </v-textarea>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn @click="closeCreateDialog">
+              取消
+            </v-btn>
+            <v-btn color="primary" @click="startCreate">
+              新增
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
     </v-card>
+    <v-dialog
+        v-model="modifyDialog"
+        max-width="1000"
+    >
+      <v-card>
+        <v-card-title>
+          編輯公告
+        </v-card-title>
+        <v-card-text>
+          <v-textarea
+            v-model="createTitle"
+            counter
+            label="公告標題"
+            rows="3"
+            auto-grow
+          >
+          </v-textarea>
+          <v-textarea
+            v-model="createContents"
+            counter
+            label="公告內容"
+            auto-grow
+          >
+          </v-textarea>
+        </v-card-text>
+      </v-card>
+    </v-dialog>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 
 export default {
   name: 'AnnounceManage',
+  data () {
+    return {
+      announcementList: [],
+      header: [
+        { text: '刪除', align: 'start', value: 'deleteAction', sortable: false },
+        { text: '公告標題', align: 'start', value: 'title', sortable: false },
+        { text: '編輯公告', align: 'end', value: 'modifyAction', sortable: false }
+      ],
+      createDialog: false,
+      modifyDialog: false,
 
-  data: () => ({
-    passwordForm: false,
-    showPassword: {
-      oldPassword: false,
-      newPassword: false,
-      confirm: false
-    },
-    password: {
-      oldPassword: '',
-      newPassword: ''
+      createTitle: '',
+      createContents: ''
     }
-  })
+  },
+  methods: {
+    openCreateDialog () {
+      this.createDialog = true
+    },
+    closeCreateDialog () {
+      this.createTitle = ''
+      this.createContents = ''
+      this.createDialog = false
+    },
+    startCreate () {
+      axios.post('/api/announcementApp/createAnnouncement', {
+        title: this.createTitle,
+        contents: this.createContents
+      })
+      this.closeCreateDialog()
+    },
+    modifyAnnouncement (item) {
+      this.modifyDialog = true
+    }
+  },
+  mounted () {
+    axios.get('/api/announcementApp/listAnnouncement').then((res) => {
+      this.announcementList = res.data.data
+    })
+  }
 }
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -30,52 +30,98 @@
             color="rgba(256,256,256,0.1)"
             elevation="6"
           >
-          <template
-            v-for="(message,i) in messages"
-          >
-            <v-list-item two-line :key="message.title">
-              <v-list-item-content>
-                <v-list-item-title>{{ message.title }}</v-list-item-title>
-                <v-list-item-subtitle>{{ message.text }}</v-list-item-subtitle>
-              </v-list-item-content>
-            </v-list-item>
-            <v-divider :key="i"></v-divider>
-          </template>
+            <v-card-title>
+              公告
+            </v-card-title>
+            <v-data-table
+              :headers="header"
+              :items="announcementList"
+              :items-per-page="5"
+              sort-by="id"
+              style="background-color:rgba(255, 255, 255, 0.0);"
+            >
+              <template v-slot:item.searchAction="{ item }">
+                <v-btn text @click="searchAnnouncement(item)">
+                  <v-icon>
+                    mdi-magnify
+                  </v-icon>
+                </v-btn>
+              </template>
+            </v-data-table>
           </v-card>
         </v-col>
       </v-row>
+      <v-dialog v-model="searchDialog" max-width="1000">
+        <v-card>
+          <v-card-text>
+            <v-textarea
+              v-model="title"
+              label="標題"
+              rows="3"
+              auto-grow
+              readonly
+            >
+            </v-textarea>
+            <v-textarea
+              v-model="contents"
+              label="內容"
+              auto-grow
+              readonly
+            >
+            </v-textarea>
+          </v-card-text>
+        </v-card>
+      </v-dialog>
     </v-container>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 
 export default {
   name: 'Home',
   data () {
     return {
       model: 0,
+      announcementList: [],
       items: [
         'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
         'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
         'https://cdn.vuetifyjs.com/images/carousel/bird.jpg',
         'https://cdn.vuetifyjs.com/images/carousel/planet.jpg'
       ],
-      messages: [
-        {
-          title: 'I am title1',
-          text: 'I am subtitle'
-        },
-        {
-          title: 'I am title2',
-          text: 'I am subtitle'
-        },
-        {
-          title: 'I am title3',
-          text: 'I am subtitle'
-        }
-      ]
+      header: [
+        { text: '標題', align: 'center', value: 'title', sortable: false },
+        { text: '詳細內容', align: 'end', value: 'searchAction', sortable: false }
+      ],
+      searchDialog: false,
+      title: '',
+      contents: ''
     }
+  },
+  methods: {
+    searchAnnouncement (item) {
+      const config = {
+        url: '/api/announcementApp/queryAnnouncement',
+        method: 'get',
+
+        param: {
+          id: item.id
+        }
+      }
+      axios(config)
+        .then((res) => {
+          this.title = res.data.data.title
+          this.contents = res.data.data.contents
+        })
+      this.searchDialog = true
+    }
+  },
+  mounted () {
+    axios.get('/api/announcementApp/listAnnouncement').then((res) => {
+      this.announcementList = res.data.data
+    })
   }
 }
 </script>
