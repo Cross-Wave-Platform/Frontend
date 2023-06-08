5b1013ef158b32b0e1db42354b26b0b9de7179da
AndrewL00 <andrew35599588@gmail.com>
2023-06-08 19:31:02 +0800

    complete page of verify email

---
+++ src/views/Verify.vue
@@ -0,0 +1,43 @@
+<template>
+<div>
+  <v-container>
+    <v-card flat style="background-color:rgba(255, 255, 255, 0.0);">
+      <v-card-title>
+          信箱認證
+      </v-card-title>
+      <v-card-text>
+        已完成信箱認證
+      </v-card-text>
+      <v-card-actions>
+        <v-btn
+        color="primary"
+        @click="$router.push('/')"
+        >
+          回到首頁
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-container>
+</div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'Verify',
+  mounted () {
+    const config = {
+      url: '/api/loginApp/user_confirm',
+      method: 'post',
+
+      data: {
+        token: this.$route.params.token
+      }
+    }
+    axios(config)
+      .then((res) => {
+      })
+  }
+}
+</script>
