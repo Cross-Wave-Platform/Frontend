a3ab9e3aa9437fa0aae9c629c7a67043d5a7a3b3
joseph89108 <joseph89108@github.com>
2023-06-21 04:02:54 +0800

    add announcement pinned switch but still with bug

--- src/components/AnnounceManage.vue
+++ src/components/AnnounceManage.vue
@@ -16,9 +16,9 @@
             </v-icon>
           </v-btn>
         </template>
-        <!--<template v-slot:item.top="{ item }">
-          {{ item.top?'已置頂':'未置頂' }}
-        </template>-->
+        <template v-slot:item.pinned="{ item }">
+          {{ item.pinned?'已置頂':'未置頂' }}
+        </template>
         <template v-slot:item.modifyAction="{ item }">
           <v-btn text @click="modifyAnnouncement(item)">
             <v-icon>
@@ -100,10 +100,9 @@
           >
           </v-textarea>
           <v-div></v-div>
-          <!-- <v-switch
-          :label="`${editedItem.top==1 ? '已置頂':'未置頂'}`"-*/
-          >
-          </v-switch>-->
+          <v-switch
+            :label="`${editedItem.pinned==1 ? '已置頂':'未置頂'}`"
+          ></v-switch>
         </v-card-text>
         <v-card-actions>
           <v-spacer></v-spacer>
@@ -170,7 +169,7 @@ export default {
       header: [
         { text: '刪除', align: 'start', value: 'deleteAction', sortable: false },
         { text: '公告標題', align: 'start', value: 'title', sortable: false },
-        //  { text: '置頂狀態', align: 'start', value: 'top', sortable: false },
+        { text: '置頂狀態', align: 'start', value: 'pinned', sortable: false },
         { text: '編輯公告', align: 'end', value: 'modifyAction', sortable: false }
       ],
       createDialog: false,
@@ -245,8 +244,8 @@ export default {
       axios.put('/api/announcementApp/updateAnnouncement', {
         id: this.editedItem.id,
         title: this.modifyTitle,
-        contents: this.modifyContents
-      //  top: (this.editedItem.top) ? 1 : 0
+        contents: this.modifyContents,
+        pinned: (this.editedItem.pinned) ? 1 : 0
       })
       this.closeModifyDialog()
     },
